bb9fca19354cb19bd603823150f90a030279868c
Vincent DUONG <vduong@e2r10p15.42.fr>
2018-08-22 18:54:15 +0200

    last commit from 42

--- srcs/fill_piece.c
+++ srcs/fill_piece.c
@@ -6,7 +6,7 @@
 /*   By: vduong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 14:41:53 by vduong            #+#    #+#             */
-/*   Updated: 2018/08/22 16:39:15 by vduong           ###   ########.fr       */
+/*   Updated: 2018/08/22 17:38:36 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,14 @@ int		fill_piece(t_piece *piece)
 	char	*line;
 	int		i;
 
-	if (get_next_line(0, &line) < 0)
-		return(1);
+	get_next_line(0, &line);
 	fill_metadata(piece, line);
 	if (!(piece->content = init_map(piece->height, piece->width)))
 		return (1);
 	i = 0;
 	while (i < piece->height)
 	{
-		if (get_next_line(0, &piece->content[i]) < 0)
+		if (get_next_line(0, &piece->content[i]) <= 0)
 			return (1);
 		i++;
 	}
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vduong <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/20 12:22:25 by vduong            #+#    #+#             */
-/*   Updated: 2018/08/22 16:38:16 by vduong           ###   ########.fr       */
+/*   Updated: 2018/08/22 18:46:38 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,25 @@ int	main()
 	t_player			player;
 	t_map				map;
 	t_piece				piece;
-	char				*firstline;
+	char				*line;
 	int					**coor;
 
-	get_next_line(0, &firstline);
-	if (strstr(firstline, "$$$"))
-		fill_player(&player, firstline);
+	if (get_next_line(0, &line) && line && ft_strlen(line) > 10 &&
+			            !ft_strncmp(line, "$$$ exec p", 9) &&
+						            (line[10] == '1' || line[10] == '2'))
+		fill_player(&player, line);
 	while (1)
 	{
 		if (fill_map(&map) == 1)
+		{
 			break;
+		}
 		if (fill_piece(&piece) == 1)
+		{
 			break;
+		}
 		coor = sorted_coor(map, piece, player.symbol);
 		printf("%d %d\n", coor[1][0], coor[1][1]);
-		//coor = get_valid_coor(map, piece, player.symbol);
-		/*i = 1;
-		printf("Il y a %d coordonnees possibles : \n", coor[0][0]);
-		while (i < coor[0][0] + 1)
-		{
-			printf("Coordonee %2d : %2d - %2d\n", i, coor[i][0], coor[i][1]);
-			i++;
-		}
-		break;
-		//sleep(44444000);
-		if (write_coor(map, piece, player) == 1)
-			break;
-		free_map(&map);
-		free_piece(&piece);*/
 	}
-	//free_player(&player);
 	return (0);
 }
